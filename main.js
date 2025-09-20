c6fc70d30d4408ff22c568cacba16d6a7a799842
Ahmed Samir <mrabureem@gmail.com>
2025-09-20 21:24:19 +0300

    version two without any constractor make it from first to end

--- main.js
+++ main.js
@@ -1,42 +1,44 @@
+const carousel = document.querySelector(".carousel");
+const list = document.querySelector(".carousel .list");
+const thumbnail = document.querySelector(".carousel .thumbnail");
 const nextBtn = document.getElementById("next");
 const prevBtn = document.getElementById("prev");
-const carousel = document.querySelector(".carousel");
-const listItem = document.querySelector(".carousel .list");
-const thumbnail = document.querySelector(".carousel .thumbnail");
 
 nextBtn.onclick = function () {
-  showSlider("next");
+  changeSlider("next");
 };
 prevBtn.onclick = function () {
-  showSlider("prev");
+  changeSlider("prev");
 };
-const timeRunning = 3000;
-const timeAutoNext = 5000;
-let runTimeOut;
-let timeAutoNextOut = setTimeout(() => {
+const timeOut = 4000;
+const autoRunTime = 7000;
+let autoRunSlider = setTimeout(() => {
   nextBtn.click();
-}, timeAutoNext);
-function showSlider(type) {
-  const itemSlider = document.querySelectorAll(".carousel .list .item");
-  const itemThumbnail = document.querySelectorAll(".carousel .thumbnail .item");
+}, autoRunTime);
 
+let removeTimeout;
+function changeSlider(type) {
+  const itemsList = document.querySelectorAll(".carousel .list .item");
+  const itemsThumbnail = document.querySelectorAll(
+    ".carousel .thumbnail .item"
+  );
   if (type === "next") {
-    listItem.appendChild(itemSlider[0]);
-    thumbnail.appendChild(itemThumbnail[0]);
+    list.appendChild(itemsList[0]);
+    thumbnail.appendChild(itemsThumbnail[0]);
     carousel.classList.add("next");
   } else {
-    const lastItemPosition = itemSlider.length - 1;
-    listItem.prepend(itemSlider[lastItemPosition]);
-    thumbnail.prepend(itemThumbnail[lastItemPosition]);
+    let lastItemIndex = itemsList.length - 1;
     carousel.classList.add("prev");
+    list.prepend(itemsList[lastItemIndex]);
+    thumbnail.prepend(itemsThumbnail[lastItemIndex]);
   }
-  clearTimeout(runTimeOut);
+  clearTimeout(removeTimeout);
   setTimeout(() => {
     carousel.classList.remove("next");
     carousel.classList.remove("prev");
-  }, timeRunning);
-  clearTimeout(timeAutoNextOut);
-  timeAutoNextOut = setTimeout(() => {
+  }, timeOut);
+  clearTimeout(autoRunSlider);
+  autoRunSlider = setTimeout(() => {
     nextBtn.click();
-  }, timeAutoNext);
+  }, autoRunTime);
 }
